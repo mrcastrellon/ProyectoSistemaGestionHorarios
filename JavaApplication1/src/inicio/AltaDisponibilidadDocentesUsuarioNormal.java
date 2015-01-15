f556c92822fdfc1c476aeb8177c16cd38b3ff85b
Benjamin <Benjamin@BENGY>
2015-01-15 11:53:23 -0600

    se realizaron acmodos en el codogo Alta Disponibilidad disponibilidad

--- JavaApplication1/src/inicio/AltaDisponibilidadDocentesUsuarioNormal.java
+++ JavaApplication1/src/inicio/AltaDisponibilidadDocentesUsuarioNormal.java
@@ -125,7 +125,7 @@ public class AltaDisponibilidadDocentesUsuarioNormal extends javax.swing.JFrame 
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(485, Short.MAX_VALUE)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(394, 394, 394))
         );

aa14d5f7674daa737f6e4c7064aba7fe3a5843a1
Benjamin <Benjamin@BENGY.lan>
2015-01-19 11:07:17 -0600

    se realizaron cambios en los cuales se agrego un lin para la pantalla modificar horario externo

--- JavaApplication1/src/inicio/HorariosExternos1.java
+++ JavaApplication1/src/inicio/HorariosExternos1.java
@@ -392,6 +392,9 @@ public class HorariosExternos1 extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        ConsultarmodificarHorariosExternos1 obj=new ConsultarmodificarHorariosExternos1();
+        obj.setVisible(true);
+        dispose(); 
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
